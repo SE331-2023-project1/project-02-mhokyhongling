c2708f1e9c146c94794683a572ce33af11bc53ad
Supanut <Supanut_m@cmu.ac.th>
2023-10-22 23:03:06 +0700

    delete all annouce

--- src/main/java/se331/project/rest/repository/AnnouceRepository.java
+++
@@ -1,7 +0,0 @@
-package se331.project.rest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se331.project.rest.entity.Annouce;
-
-public interface AnnouceRepository extends JpaRepository<Annouce,Long> {
-}
